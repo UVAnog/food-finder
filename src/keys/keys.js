d16ca12f2ead0185be5d55fd55c293bcdffdae3f
Nolan Harris <nph2tx@virginia.edu>
2021-05-24 13:14:58 -0400

    Update keys.js

--- src/keys/keys.js
+++ src/keys/keys.js
@@ -1,4 +1,4 @@
-const API_KEY = "AIzaSyBb98m494N57njrw1bGS3LUlwGLTP3fWy0";
-const MAP_KEY = "pk.eyJ1Ijoibm9sYW5oYXJyaXMtZGV2IiwiYSI6ImNrb3g1ZTB4eDBheGQydnF3dnhrZXczNXcifQ.rDO3d4v4mc0SSUFQCsaaEA";
-
+const API_KEY = process.env.API_KEY;
+const MAP_KEY = process.env.MAP_KEY
 export { API_KEY, MAP_KEY };
+
